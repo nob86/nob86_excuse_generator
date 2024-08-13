3c29321d25092369fc33b66144bc11225dd7e4fe
nob86 <gustavoadallacroce@gmail.com>
2024-08-13 16:05:38 +0000

    generador de excusas

--- src/app.js
+++ src/app.js
@@ -7,6 +7,36 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  function generator_excuse() {
+    const generateRandomArray = array => {
+      return Math.floor(Math.random() * array.length);
+    };
+    let randomWho = generateRandomArray(who);
+    let randomAction = generateRandomArray(action);
+    let randomWhat = generateRandomArray(what);
+    let randomWhen = generateRandomArray(when);
+
+    let excuse =
+      who[randomWho] +
+      " " +
+      action[randomAction] +
+      " " +
+      what[randomWhat] +
+      " " +
+      when[randomWhen];
+
+    return excuse;
+  }
+  document.getElementById("excuse").innerHTML = generator_excuse();
 };
-document.g;
